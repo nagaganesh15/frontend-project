7383fac8ffd92f57a127fee132137b7be2d710f4
M.Naga Ganesh <nagaganesh.mullangi@gmail.com>
2025-06-07 17:45:17 +0530

    Updated Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/AllGames/",
+  base: "/frontend-project/", // Update to match GitHub Pages repo name
   plugins: [react()]
 })
